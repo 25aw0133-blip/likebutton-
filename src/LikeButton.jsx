26bb4b813f0812faa083c73e6751aef26d6e8284
25aw0133-blip <25aw0133@jec.ac.jp>
2026-02-27 11:33:26 +0900

    Initial commit

---
+++ src/LikeButton.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function LikeButton() {
+  const [likes, setLikes] = useState(0);
+
+  return (
+    <div>
+      <p>❤️ {likes}</p>
+      <button onClick={() => setLikes(likes + 1)}>+1</button>
+    </div>
+  );
+}
+
+export default LikeButton;
